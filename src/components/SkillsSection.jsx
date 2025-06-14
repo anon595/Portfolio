be1b4e50c7503732915af0a4fd38a98a11a4ff3a
anon595 <pedrojunior20011227@gmail.com>
2025-06-14 13:49:28 -0700

    Atualizacao no tema

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -78,7 +78,7 @@ export const SkillsSection = () => {
                 <img
                   src={`/Skills/${skill.icon}`}
                   alt={skill.name}
-                  className="w-6 h-6 filter dark:invert dark:brightness-100"
+                  className="w-6 h-6 transition-all duration-300 dark:invert dark:brightness-[2]"
                 />
               </div>
 
